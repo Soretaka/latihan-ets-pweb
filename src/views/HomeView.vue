0a40fdb6447f8f7127c74de6c39e959d3abe31a2
soretaka <yatesthap@gmail.com>
2022-11-04 07:55:07 +0700

    todo show

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div v-for="todo in todos" :key="todo.id">
+      {{ todo.name }}
+      {{ todo.description }}
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { collection, getDocs } from "firebase/firestore";
+import db from "../firebase";
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  mounted() {
+    getDocs(collection(db, "todos")).then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        this.todos.push(doc.data());
+      });
+    });
   },
 };
 </script>
